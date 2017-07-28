1e4392f0f73fd9bc504e4599a772f9b0d2f7ba25
Juan Manuel <juanito.lg@gmail.com>
2017-07-28 00:32:55 -0600

    Create actions, add Touchable and expanded list

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
+
+  renderDescription() {
+    const { library, selectedLibraryId } = this.props;
+    const { descriptionStyle } = styles;
+    
+    if (library.id === selectedLibraryId) {
+      return (
+        <Text style={descriptionStyle}>{library.description}</Text>
+      );
+    }
+  }
+
   render() {
     const { titleStyle } = styles;
+    const { id, title } = this.props.library;
+
     return (
-      <CardSection>
-        <Text style={titleStyle}>
-          {this.props.library.title}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+        <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -19,7 +42,16 @@ const styles = {
   titleStyle: {
     fontSize: 18,
     paddingLeft: 15
+  },
+  descriptionStyle: {
+    paddingLeft: 15,
+    fontSize: 16,
+    color: 'red'
   }
 };
 
-export default ListItem;
+const mapStateToProps = state => {
+  return { selectedLibraryId: state.selectedLibraryId };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
